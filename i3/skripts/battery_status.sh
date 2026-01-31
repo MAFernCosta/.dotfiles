32b858911842340960bc29cd61f0b796c2347840
Miguel <123456789+asrahdr@gmail.com>
2026-01-31 18:16:14 +0100

    i3block skript to show battery status

---
+++ i3/skripts/battery_status.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Labels
+charging="Chr"
+notCharging="Con"
+disCharging="Dis"
+unknown="?!?"
+
+percentage=$(acpi -b | grep "Battery 0" | cut -d ":" -f 2 | cut -d "," -f 2)
+status=$(acpi -b | grep "Battery 0" | cut -d ":" -f 2 | cut -d "," -f 1)
+curStatus="NR"
+
+case $status in 
+    " Not Charging")
+        curStatus=$notCharging
+        ;;
+    " Discharging")
+        curStatus=$disCharging
+        ;;
+    " Charging")
+        curStatus=$charging
+        ;;
+    *)
+        curStatus="NR"
+        ;;
+esac
+
+echo "$percentage $curStatus"
+
