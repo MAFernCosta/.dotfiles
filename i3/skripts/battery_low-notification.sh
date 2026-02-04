1added5e4961184e6e8ffa16e56fe1a9ba16804c
Miguel <123456789+asrahdr@gmail.com>
2026-02-04 20:23:12 +0100

    Added low battery notifications

--- i3/skripts/battery_low-notification.sh
+++ i3/skripts/battery_low-notification.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
+# Date: 04.02.26
 
-BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
+set -u
+
+notif=20
+
+hiber=15
+
+# Path for the internal battery it can be BAT0 or BAT1
+BAT_PATH="/sys/class/power_supply/BAT0"
+
+# In case BAT0 doesnt exits swap to BAT1
+if [ ! -d "$BAT_PATH" ]; then
+    BAT_PATH="/sys/class/power_supply/BAT1"
 fi
+
+percentage=$(cat "$BAT_PATH/capacity")
+status=$(cat "$BAT_PATH/status")
+if [ $status = 'Discharging' ] && [ $percentage -le $notif ] && [ $percentage -gt $hiber ]
+then
+    notify-send -u critical "Battery warning" "Battery at $percentage%. Connect the power adapter!"
+elif [ $status = 'Discharging' ] && [ $percentage -le $hiber ]
+then
+    ID=9999
+    for i in {60..1}; do
+        dunstify -r $ID "Battery too low" "System will shutdown in $i seconds. Connect the power adapter to cancel this action." -u critical
+        status=$(cat "$BAT_PATH/status")
+        if [ $status = 'Charging' ]
+        then
+            dunstify -C $ID
+            exit
+        fi
+        sleep 1
+    done
+
+    dunstify -r $ID "Shutdown warning" "Shutdown now"
+    sleep 5
+    systemctl suspend
+fi
+
+
